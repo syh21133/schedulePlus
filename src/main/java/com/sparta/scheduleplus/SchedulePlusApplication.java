14a1e9eea04728458a9d3c6e6e26254a3767dcb8
syh213 <syh21133@gmail.com>
2024-10-15 10:06:37 +0900

    init commit

---
+++ src/main/java/com/sparta/scheduleplus/SchedulePlusApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.scheduleplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulePlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/scheduleplus/SchedulePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.scheduleplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulePlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
